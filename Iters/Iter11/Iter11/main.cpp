e7e36a1a7d39f5968d78c0ac178baa925ae143b7
Yhondri <yhondri@gmail.com>
2016-11-20 21:50:21 +0100

    new: Finished Iter 11. Success.

--- Iters/Iter11/Iter11/main.cpp
+++ Iters/Iter11/Iter11/main.cpp
@@ -7,9 +7,58 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+bool resuelve();
+bool parImpar(int v[], int n, bool &existe, int &position);
+
+int main() {
+    while (resuelve());
     return 0;
+}
+
+bool parImpar(int v[], int n, bool &existe, int &position) {
+    bool par = true;
+    
+    for (int i = 0; i < n; i++) {
+        if (!par && existe) {
+            if (v[i] % 2 == 0) {
+                existe = false;
+            }
+        } else {
+            if (v[i] % 2 != 0) {
+                par = false;
+                position = i;
+            }
+        }
+    }
+    if (par) {
+        position = n;
+    }
+    return existe;
+}
+
+
+bool resuelve() {
+    // Leer datos
+    int n;
+    int max;
+    
+    cin >> max;
+    for (int j = 0; j < max; j++) {
+        cin >> n;
+        int v[n];
+        for (int i=0; i<n; i++) {
+            cin >> v[i];
+        }
+        
+        int position = 0;
+        bool existe = true;
+        if (parImpar(v, n, existe, position)) {
+            cout << "Si " << position << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+    return false;
 }
