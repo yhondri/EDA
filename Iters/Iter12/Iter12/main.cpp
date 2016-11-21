d2ec13b9af8f0223ef77f42064fca26884fab3c3
Yhondri <yhondri@gmail.com>
2016-11-21 20:26:55 +0100

    new: Added gitignore.

---
+++ Iters/Iter12/Iter12/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Iter12
+//
+//  Created by Yhondri on 21/11/16.
+//  Copyright © 2016 Yhondri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
