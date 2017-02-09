b6ce63a9bd720a326374116813bac18ffad33a35
Yhondri <yhondri@gmail.com>
2017-02-09 12:31:49 +0100

    new: Added VA06 and SUCCESS.

--- Iters/VA06/VA06/main.cpp
+++ Iters/VA06/VA06/main.cpp
@@ -1,116 +1,89 @@
 //
 //  main.cpp
-//  VA06
+//  Backtraking01
 //
-//  Created by Yhondri on 12/1/17.
+//  Created by Yhondri on 9/2/17.
 //  Copyright © 2017 Yhondri. All rights reserved.
 //
 
 #include <iostream>
 using namespace std;
 
-void calcularMejorCoste(int** products, int* productosCompradosEnSuper, int* solucion, int rows, int columns, int k, int& mejorcoste);
-bool esValida(int* productosCompradosEnSuper, int i);
-bool esSolucion(int k, int nSupermercados);
-int calcularCoste(int** products, int* solucion, int rows, int columns);
-void fillArray(int** products, int rows, int columns);
-void output(int** products, int rows, int columns);
+bool esValida(int* supermercados, int k);
+bool esSolucion(int k, int productos);
+int valorDeCesta(int* solucion, int n);
+int vueltaAtras(int** productos, int filas, int columnas, int* supermercados, int* solucion, int k, int &minCoste);
+void visualizarDatos(int** productos, int filas, int columnas);
 
 int main(int argc, const char * argv[]) {
-    int numberOfCases = 0;
-    cout <<"Introduce nº casos"<< endl;
-    cin >> numberOfCases;
-    int i = 0;
-    while (i < numberOfCases) {
-        int rows = 0;
-        int columns = 0;
-        cout <<"Introduce filas"<< endl;
-        cin >> rows;
-        cout <<"Introduce columnas"<< endl;
-        cin >> columns;
-        int** products = new int*[rows];
-        int solution[columns];
-        int productosCompradosEnSuper[rows];
-        for(int i = 0; i < rows; ++i){
-            products[i] = new int[columns];
+    
+    int numCasos = 0;
+    cout << "Introduce numCasos " << endl;
+    cin >> numCasos;
+    while (numCasos > 0) {
+        int filas = 0;
+        int columnas = 0;
+        cin >> filas;
+        cin >> columnas;
+        int** productos = new int *[filas];
+        for (int i = 0; i < filas; i++) {
+            productos[i] = new int[columnas];
         }
-        int mejorcoste = INT_MAX;
-        fillArray(products, rows, columns);
-//        output((int **)products, rows, columns);
-        calcularMejorCoste(products, productosCompradosEnSuper, solution, rows, columns, 0, mejorcoste);
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                cin >> productos[i][j];
+            }
+        }
         cout << endl;
-        cout << "Mejor coste: " << mejorcoste << endl;
-        i++;
+        int* supermercados = new int[filas];
+        int* solucion = new int[columnas];
+        int minCoste = INT_MAX;
+        cout << vueltaAtras(productos, filas, columnas, supermercados, solucion, 0, minCoste) << endl;
+        numCasos--;
     }
     return 0;
 }
 
-/**
- - Products = LLeva todos los productos en los diferentes súpermercados. 
- - Solución: LLeva la solución que se está evaluando. 
- - Rows: Número de súpermercados. 
- - Columns: Número de productos.
- */
-void calcularMejorCoste(int** products, int* productosCompradosEnSuper, int* solucion, int rows, int columns, int k, int& mejorcoste){
-    for (int i = 0; i < rows; i++) {
-        if (esValida(productosCompradosEnSuper, i)) {
-            
-        }
-        
-        
-//        solucion[k] = i;
-//        
-//        for (int index = k+1; index < columns; index++) {
-//            solucion[index] = 0;
-//        }
-//        
-//        if (esValida(solucion, columns, k)) {
-//            if (esSolucion(k, rows)) {
-//                int costeSolucion = calcularCoste(products, solucion, rows, columns);
-//                if (costeSolucion < mejorcoste) {
-//                    mejorcoste = costeSolucion;
-//                }
-//            }else {
-//                calcularMejorCoste(products, productosCompradosEnSuper, solucion, rows, columns, k+1, mejorcoste);
-//            }
-//        }
-    }
-}
-
-/**
- - Mira si hay más de 3 compras en el mismo súpermercado.
- */
-bool esValida(int* productosCompradosEnSuper, int i) {
-    return !(productosCompradosEnSuper[i] > 3);
-}
-
-bool esSolucion(int k, int nSupermercados) {
-    return k == nSupermercados - 1;
-}
-
-int calcularCoste(int** products, int* solucion, int rows, int columns) {
-    int coste = 0;
-    for (int i = 0; i < columns; i++) {
-        cout << products[solucion[i]][i] << " + ";
-        coste += products[solucion[i]][i];
-    }
-    cout << endl;
-    return coste;
-}
-
-void fillArray(int** products, int rows, int columns) {
-    for(int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cin >> products[i][j];
+int vueltaAtras(int** productos, int filas, int columnas, int* supermercados, int* solucion, int k, int &minCoste) {
+    for (int i = 0; i < filas; i++) {
+        if (esValida(supermercados, i)) {
+            supermercados[i]++;
+            solucion[k] = productos[i][k];
+            if (esSolucion(k, columnas)) {
+                int nuevoCoste = valorDeCesta(solucion, columnas);
+                if (nuevoCoste < minCoste) {
+                    minCoste = nuevoCoste;
+                }
+            }else {
+                vueltaAtras(productos, filas, columnas, supermercados, solucion, k + 1, minCoste);
+            }
+            supermercados[i]--;
+            solucion[k] -= productos[i][k];
         }
     }
+    return minCoste;
 }
 
-void output(int** products, int rows, int columns) {
-    cout <<"Array cargado: "<< endl;
-    for(int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cout << products[i][j] << " ";
+bool esValida(int* supermercados, int k) {
+    return supermercados[k] < 3;
+}
+
+bool esSolucion(int k, int productos) {
+    return k == productos - 1;
+}
+
+int valorDeCesta(int* solucion, int n) {
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        total += solucion[i];
+    }
+    return total;
+}
+
+void visualizarDatos(int** productos, int filas, int columnas){
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            cout << productos[i][j] << " ";
         }
         cout << endl;
     }
