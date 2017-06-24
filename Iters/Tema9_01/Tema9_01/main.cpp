b4a7f658d49155bce83bf05a9740bd3ed4903d97
Yhondri <yhondri@gmail.com>
2017-06-24 20:51:03 +0200

    new: Added Tema9_01

--- Iters/Tema9_01/Tema9_01/main.cpp
+++ Iters/Tema9_01/Tema9_01/main.cpp
@@ -8,32 +8,83 @@
 
 #include <iostream>
 #include "Arbin.h"
+#include "List.h"
 
 using namespace std;
 
-Arbin<char> leerArbol();
+void fillList(List<int> &arbinList);
+Arbin<int> crearArbol(List<int> inOrden, List<int> preOrden);
+Arbin<int> crearArbolAux(List<int> inOrden, List<int> preOrden, int inOrdenStart, int inOrdenEnd);
 
-int main() {
-    int numCasos = 0;
-    cin >> numCasos;
-    while (numCasos > 0) {
-        Arbin<char>arbin = leerArbol();
-        cout << arbin.numNodos() << " " << arbin.numHojas() << " " << arbin.altura() << endl;
-        numCasos--;
+int main()
+{
+    List<int> listaPreorden;
+    List<int> listaInorden;
+    fillList(listaPreorden);
+    fillList(listaInorden);
+    
+    while (!listaPreorden.empty() && !listaInorden.empty()) {
+        
+        Arbin<int> result = crearArbol(listaInorden, listaPreorden);
+        List<int>::Iterator iterator = result.niveles()->begin();
+        while (iterator != result.niveles()->end()) {
+            cout << iterator.elem() << " ";
+            iterator.next();
+        }
+        listaPreorden = List<int>();
+        listaInorden = List<int>();
+        fillList(listaPreorden);
+        fillList(listaInorden);
     }
     
     return 0;
 }
 
-Arbin<char> leerArbol() {
-    char raiz;
-    cin >> raiz;
-    if (raiz == '.'){
-        return Arbin<char>();
+
+Arbin<int> crearArbol(List<int> inOrden, List<int> preOrden) {
+    return crearArbolAux(inOrden, preOrden, 0, inOrden.size() - 1);
+}
+
+Arbin<int> crearArbolAux(List<int> inOrden, List<int> preOrden, int inOrdenStart, int inOrdenEnd) {
+    
+    static int preIndex = 0;
+    
+    if (inOrdenStart > inOrdenEnd) {
+        return Arbin<int>();
     }
-    else {
-        Arbin<char> izquierdo = leerArbol();
-        Arbin<char> derecho = leerArbol();
-        return Arbin<char>(izquierdo, raiz, derecho);
+    
+    if (inOrdenStart == inOrdenEnd) {
+        return Arbin<int>(Arbin<int>(), preOrden.at(preIndex++), Arbin<int>());
     }
+    
+    int inOrdenValue = preOrden.at(preIndex++); //Probablemente se pueda mejorar utilizando iteradores.
+    int inOrdenIndex;
+    for (int i = inOrdenStart; i <= inOrdenEnd; i++) {
+        if (inOrdenValue == inOrden.at(i)) {
+            inOrdenIndex = i;
+            break;
+        }
+    }
+    
+    Arbin<int> izquierdo = crearArbolAux(inOrden, preOrden, inOrdenStart, inOrdenIndex - 1);
+    Arbin<int> derecho = crearArbolAux(inOrden, preOrden, inOrdenIndex + 1, inOrdenEnd);
+    
+    return Arbin<int>(izquierdo, inOrdenValue, derecho);
+}
+
+void fillList(List<int> &arbinList) {
+    int num;
+    while ((cin >> num) && num != -1) {
+        arbinList.push_back(num);
+    }
+//    
+//    for (int i = 0; i < arbinList.size(); i++) {
+//        cout << arbinList.at(i) << " ";
+//    }
+//    cout << endl;
+//    cout << "--------" << endl;
+//    cout << endl;
+//    cout << endl;
+//    cout << endl;
+
 }
