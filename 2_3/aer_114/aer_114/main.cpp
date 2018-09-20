eec3b292efac605448872e8d96123581c90d0608
Yhondri Josué Acosta Novas <yhondri@gmail.com>
2018-09-20 17:38:34 +0200

    Add aer_114 exercise's files.

--- 2_3/aer_114/aer_114/main.cpp
+++ 2_3/aer_114/aer_114/main.cpp
@@ -7,9 +7,53 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    // ajustes para que cin extraiga directamente de un fichero
+//#ifndef DOMJUDGE
+//    ifstream in("/Users/yhondri/Documents/University/eda/2_3/aer_114/aer_114/casos");
+//    auto cinbuf = cin.rdbuf(in.rdbuf());
+//#endif
+
+    int numCasos = 0;
+    cin >> numCasos;
+
+    while (numCasos > 0) {
+        int num;
+        cin >> num;
+
+        switch (num) {
+            case 0:
+                cout << 1 << "\n";
+                break;
+            case 1:
+                cout << 1 << "\n";
+                break;
+            case 2:
+                cout << 2 << "\n";
+                break;
+            case 3:
+                cout << 6 << "\n";
+                break;
+            case 4:
+                cout << 4 << "\n";
+                break;
+            default:
+                cout << 0 << "\n";
+                break;
+        }
+
+        numCasos--;
+    }
+
+    // para dejar todo como estaba al principio
+//#ifndef DOMJUDGE
+//    cin.rdbuf(cinbuf);
+//    //    system("PAUSE");
+//#endif
+
     return 0;
 }
