0f981bfc62de9808a11bc5be800a2627d43cc5b5
Yhondri Josué Acosta Novas <yhondri@gmail.com>
2018-09-20 17:35:35 +0200

    Add aer114.

---
+++ 2_3/aer_114/aer_114/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  aer_114
+//
+//  Created by Yhondri Josué Acosta Novas on 20/09/2018.
+//  Copyright © 2018 yhondri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
