b9c2702779be924a48f7081d4dfa595f3ccbbb88
Yhondri <yhondri@gmail.com>
2018-09-23 12:00:56 +0200

    Add aer_166

---
+++ 2_3/aer_116/aer_116/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  aer_116
+//
+//  Created by Yhondri  on 23/9/18.
+//  Copyright © 2018 Yhondri . All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int numCasos;
+    cin >> numCasos;
+    
+    while (numCasos > 0) {
+        cout << "Hola mundo.\n";
+        numCasos--;
+    }
+    return 0;
+}
