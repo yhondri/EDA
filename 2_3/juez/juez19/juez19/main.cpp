6f24e06cf0aa5644543601f06f884009329860c3
Yhondri <yhondri@gmail.com>
2018-11-14 21:25:22 +0100

    Nuevos cambios.

--- 2_3/juez/juez19/juez19/main.cpp
+++ 2_3/juez/juez19/juez19/main.cpp
@@ -9,18 +9,13 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <limits.h>
 
 using namespace std;
 
 bool leerDatos(vector<int> &datos);
 bool estaParcialMenteOrdeando(vector<int> &datos);
-bool estaParcialMenteOrdeando(vector<int> &datos,
-                              int minIzq,
-                              int maxIz,
-                              int minDer,
-                              int maxDer,
-                              int i,
-                              int j);
+bool estaParcialMenteOrdeando(vector<int> &datos, int min, int max);
 
 bool estaParcialMenteOrdeando(vector<int> &datos) {
     if (datos.size() == 2) {
@@ -30,22 +25,30 @@ bool estaParcialMenteOrdeando(vector<int> &datos) {
             return false;
         }
     } else {
-        int minIzq = INT_MAX, maxIzq = INT_MIN, minDer = INT_MAX, maxDer = INT_MIN;
-        int i = 0, j = (int)datos.size()-1;
-        return estaParcialMenteOrdeando(datos, minIzq, maxIzq, minDer, maxDer, i, j);
+        int mitad = ((int)datos.size()-1)/2;
+        bool mitadIzquierda = estaParcialMenteOrdeando(datos, 0, mitad);
+        bool mitadDerecha = estaParcialMenteOrdeando(datos, mitad+1, ((int)datos.size()-1));
+        return mitadIzquierda && mitadDerecha;
     }
 }
 
-bool estaParcialMenteOrdeando(vector<int> &datos, int minIzq, int maxIz, int minDer, int maxDer, int i, int j) {
-
-
-    return false;
+bool estaParcialMenteOrdeando(vector<int> &datos, int min, int max) {
+    if (datos[min] > datos[max]) {
+        return false;
+    }
+    
+    if (min == max-1) {
+        return true;
+    }
+    
+    return estaParcialMenteOrdeando(datos, min+1, max-1);
 }
 
 int main(int argc, const char * argv[]) {
     //            ajustes para que cin extraiga directamente de un fichero
 #ifndef DOMJUDGE
-    ifstream in("/Users/yhondri/Documents/universidad/eda/eda/2_3/juez/juez19/juez19/casos");
+    ifstream in("/Users/admin/Documents/universidad/eda/2_3/juez/juez19/juez19/casos");
+//    ifstream in("/Users/yhondri/Documents/universidad/eda/eda/2_3/juez/juez19/juez19/casos");
     auto cinbuf = cin.rdbuf(in.rdbuf());
 #endif
 
@@ -55,8 +58,14 @@ int main(int argc, const char * argv[]) {
         vector<int> datos;
         seguir = leerDatos(datos);
 
+        if (seguir) {
+            if (estaParcialMenteOrdeando(datos)) {
+                cout << "SI" << endl;
+            } else {
+                cout << "NO" << endl;
+            }
+        }
     }
-
 
 #ifndef DOMJUDGE
     cin.rdbuf(cinbuf);
@@ -68,15 +77,9 @@ int main(int argc, const char * argv[]) {
 
 bool leerDatos(vector<int> &datos) {
     int nuevoDato = 0, i = 0;
-    bool seguir = true;
-
-    while (cin >> nuevoDato && nuevoDato > 0 && seguir) {
-        if (nuevoDato == 0 && i == 0) {
-            seguir = false;
-        } else {
-            datos.push_back(nuevoDato);
-            i++;
-        }
+    while (cin >> nuevoDato && nuevoDato > 0) {
+        datos.push_back(nuevoDato);
+        i++;
     }
-    return  seguir;
+    return (nuevoDato == 0 && i > 0);
 }
