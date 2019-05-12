68895bf5aae2418432123e3606db4785f44091dd
Yhondri <yhondri@gmail.com>
2019-05-12 20:40:27 +0200

    juez31 correct)

--- 2_3/juez/2_cuatrimestre/juez31/juez31/main.cpp
+++ 2_3/juez/2_cuatrimestre/juez31/juez31/main.cpp
@@ -12,6 +12,7 @@
 #include <set>
 #include <algorithm>
 #include <map>
+#include <ctype.h>
 
 using namespace std;
 
@@ -19,68 +20,60 @@ void toLower(string &word) {
     std::transform(word.begin(), word.end(), word.begin(), ::tolower);
 }
 
-bool resolverCaso() {
+struct Nada {};
+
+void resuelveCaso() {
     int numLineas;
-    cin >> numLineas;
-    
-    if (!cin || numLineas == 0) {
-        return false;
-    }
-    
-    string temp;
-    getline(cin, temp); //Para limpiar la primera línea.
-    
-    
-    string line, clave;
-    map<string, set<int>> palabras;
-    
-    for(int i = 1; i <= numLineas; i++) {
-        getline(cin, line);
-        istringstream iss(line);
+    scanf("%d", &numLineas);
+    while( numLineas != 0){
+        char removeWhiteLine;
+        scanf("%c", &removeWhiteLine);
         
-        while (iss >> clave) {
-            if (clave.length() > 2) {
-                
-                toLower(clave);
-                
-                palabras[clave].insert(i);
+        map<string, map<int, Nada>> wordsMap;
+        
+        for (int i = 1; i <= numLineas; i++) {
+            string str;
+            getline(cin, str);
+            
+            stringstream ss(str);
+            string word;
+            while(ss >> word){
+                if(word.length() > 2) {
+                    for(unsigned int j=0; j<word.length(); j++){
+                        word[j] = tolower(word[j]);
+                    }
+                    
+                    wordsMap[word][i];
+                }
             }
         }
-    }
-    
-    for(const auto& value : palabras) {
-        cout << value.first;
-        
-        for (int page : value.second) {
-            cout << " " << page;
+        for (auto it = wordsMap.cbegin(); it != wordsMap.cend(); ++it){
+            printf("%s", (*it).first.c_str() );
+            for (auto linesIt = (*it).second.cbegin(); linesIt != (*it).second.cend(); ++linesIt){
+                printf(" %d", (*linesIt).first );
+            }
+            printf("%s\n", "" );
         }
+        printf("%s\n", "----" );
         
-        cout << "\n";
+        scanf("%d", &numLineas );
     }
-    
-    cout << "----\n";
-    
-    //Limpiamos última línea.
-    //    cin >> numLineas;
-    getline(cin, temp);
-    
-    return true;
 }
 
 int main(int argc, const char * argv[]) {
     //            ajustes para que cin extraiga directamente de un fichero
-#ifndef DOMJUDGE
-
-    ifstream in("/Users/yhondri/Documents/universidad/eda/2_3/juez/2_cuatrimestre/juez31/juez31/casos");
-    auto cinbuf = cin.rdbuf(in.rdbuf());
-#endif
+//#ifndef DOMJUDGE
+//
+//    ifstream in("/Users/yhondri/Documents/universidad/eda/2_3/juez/2_cuatrimestre/juez31/juez31/casos");
+//    auto cinbuf = cin.rdbuf(in.rdbuf());
+//#endif
 ////
-    while (resolverCaso()) {}
-////
-#ifndef DOMJUDGE
-    cin.rdbuf(cinbuf);
-//        system("PAUSE");
-#endif
+    resuelveCaso();
+    ////
+//#ifndef DOMJUDGE
+//    cin.rdbuf(cinbuf);
+////        system("PAUSE");
+//#endif
     
     return 0;
 }
